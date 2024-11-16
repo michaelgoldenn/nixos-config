0efc4ad15e70561afeafbb5d2b37f596137bcf07
michaelgoldenn <johndoe@nixos.com>
2024-11-16 11:25:54 -0500

    another test

--- modules/home/firefox.nix
+++ modules/home/firefox.nix
@@ -1,49 +1,13 @@
-# home.nix 
-                                                                                           
-{ pkgs, inputs, ... }:
-
+{ pkgs, ... }:
 {
+  home-manager.users.michael = {
+    dconf.settings = {
+      "org/gnome/shell" = {
+        favorite-apps = ["firefox.desktop"]
+      }
+    };
+  };
   programs.firefox = {
     enable = true;
-    profiles.michael = {
-
-      search.engines = {
-        "Nix Packages" = {
-          urls = [{
-            template = "https://search.nixos.org/packages";
-            params = [
-              { name = "type"; value = "packages"; }
-              { name = "query"; value = "{searchTerms}"; }
-            ];
-          }];
-
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@np" ];
-        };
-      };
-      search.force = true;
-
-      bookmarks = [
-        {
-          name = "wikipedia";
-          tags = [ "wiki" ];
-          keyword = "wiki";
-          url = "https://en.wikipedia.org/wiki/Special:Search?search=%s&go=Go";
-        }
-      ];
-
-      settings = {
-        "dom.security.https_only_mode" = true;
-        "browser.download.panel.shown" = true;
-        "identity.fxaccounts.enabled" = false;
-        "signon.rememberSignons" = false;
-      };
-
-      userChrome = ''                         
-        /* some css */                        
-      '';                                      
-
-
-    };
   };
 }
