3e8cae0ed88fd6f1c2617a05d3c670fdde883066
michaelgoldenn <johndoe@nixos.com>
2024-11-18 16:45:19 -0500

    moved spotify back because I need some music rn

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -2,6 +2,10 @@
 {
   dconf = {
     enable = true;
-    settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    settings = {
+      #"/org/gnome/desktop/interface/clock-format" = "12h";
+      #"/org/gtk/settings/file-chooser/clock-format" = "12h";
+      "org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    };
   };
 }
