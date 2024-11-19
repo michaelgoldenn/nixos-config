2f14dcba62b7f5e52ba902e9a54674f70211f97b
michaelgoldenn <johndoe@nixos.com>
2024-11-18 19:53:30 -0500

    I just got an error message that said I need to make it pure so here we go

--- modules/nixos/gui/stylix.nix
+++ modules/nixos/gui/stylix.nix
@@ -4,10 +4,13 @@ let
   inherit (inputs) self;
 in
 {
+  imports = [
+    flake.inputs.stylix.nixosModules.stylix
+  ];
   stylix = {
     enable = true;
     image = /home/michael/Downloads/thank_you_wallpaper.png;
     polarity = "dark";
-    cursor.size = 320
+    cursor.size = 320;
   };
 }
