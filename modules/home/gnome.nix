29230f7d028bd13db3fe3e2a3bb8dd05d44f1b07
michaelgoldenn <johndoe@nixos.com>
2024-11-19 18:09:13 -0500

    working on stylix again, wish me luck

--- modules/home/gnome.nix
+++ modules/home/gnome.nix
@@ -3,11 +3,19 @@
   dconf = {
     enable = true;
     settings = {
-      #"/org/gnome/desktop/interface/clock-format" = "12h";
-      #"/org/gtk/settings/file-chooser/clock-format" = "12h";
+      "org/gnome/desktop/interface".clock-format = "12h";
+      #"org/gtk/settings/file-chooser".clock-format = "12h";
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
       "org/gnome/shell" = {
-        favorite-apps = ["firefox.desktop" "vesktop.desktop" "code.desktop"];
+        favorite-apps = ["firefox.desktop" "code.desktop" "vesktop.desktop"];
+      };
+      "org/gnome/settings-daemon/plugins/media-keys" = {
+        media = ["<Shift><Control><Alt>q"]; # opens media player
+        next = ["<Shift><Control><Alt>Right"];
+        play = ["<Shift><Control><Alt>space"];
+        previous = ["<Shift><Control><Alt>Left"];
+        down = ["<Shift><Control><Alt>Down"];
+        up = ["<Shift><Control><Alt>Up"];
       };
     };
   };
--- modules/nixos/gui/stylix.nix
+++ modules/nixos/gui/stylix.nix
@@ -5,15 +5,16 @@ let
 in
 {
 # temp steam stuff, delet later
-/*   imports = [
+  imports = [
     flake.inputs.stylix.nixosModules.stylix
   ];
   stylix = {
     enable = true;
-    image = pkgs.fetchurl {
+/*     image = pkgs.fetchurl {
       url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
       sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
-    };
+    }; */
+    image = ./wallpaper.png;
     polarity = "dark";
-  }; */
+  };
 }
