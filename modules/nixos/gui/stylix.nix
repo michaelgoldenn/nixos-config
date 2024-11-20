75fb6b5f77e5e1728d81b7264150b37b3b3d37f8
michaelgoldenn <Michael.Golden0278@gmail.com>
2024-11-20 00:36:16 -0500

    titania online

--- modules/nixos/gui/stylix.nix
+++ modules/nixos/gui/stylix.nix
@@ -15,7 +15,26 @@ in
       sha256 = "sha256-k2ABo3TOhj52vocn0ksNHuOba2F6uoT19WX+w1Rr6ls=";
     };
     # themes defined here: https://tinted-theming.github.io/base16-gallery/
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catpucchin-frappe.yaml";
+    #base16Scheme = "${pkgs.base16-schemes}/share/themes/catpucchin-frappe.yaml";
+    # for some reason it's breaking when I try to import like that - I'll import manually instead.
+    base16Scheme = {
+        base00 = "#303446"; # base
+        base01 = "#292c3c"; # mantle
+        base02 = "#414559"; # surface0
+        base03 = "#51576d"; # surface1
+        base04 = "#626880"; # surface2
+        base05 = "#c6d0f5"; # text
+        base06 = "#f2d5cf"; # rosewater
+        base07 = "#babbf1"; # lavender
+        base08 = "#e78284"; # red
+        base09 = "#ef9f76"; # peach
+        base0A = "#e5c890"; # yellow
+        base0B = "#a6d189"; # green
+        base0C = "#81c8be"; # teal
+        base0D = "#8caaee"; # blue
+        base0E = "#ca9ee6"; # mauve
+        base0F = "#eebebe"; # flamingo
+    };
     /* Themes I've tried:
     catpucchin-frappe - comments are kinda hard to read
     
