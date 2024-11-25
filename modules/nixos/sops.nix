1e8d36d03800bbcf299c388cbf769b2c9c870e81
michael <Michael.Golden0278@gmail.com>
2024-11-24 23:48:29 -0500

    sops

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -4,23 +4,15 @@ let
   inherit (inputs) self;
 in
 {
-  imports = [inputs.sops-nix.nixosModules.default];
+  imports = [/* inputs.sops-nix.nixosModules.sops */ inputs.sops-nix.nixosModules.default];
 
   sops = {
-/*     # This will add secrets.yml to the nix store
-    # You can avoid this by adding a string to the full path instead, i.e.
-    # sops.defaultSopsFile = "/root/.sops/secrets/example.yaml";
     defaultSopsFile = ../../secrets/example.yaml;
-    # This will automatically import SSH keys as age keys
-    age.sshKeyPaths = [ "/etc/ssh/ssh_host_ed25519_key" ];
-    # This is using an age key that is expected to already be in the filesystem
-    age.keyFile = "/var/lib/sops-nix/key.txt";
-    # This will generate a new key if the key specified above does not exist
-    age.generateKey = true;
-    # This is the actual specification of the secrets.
-    secrets.example-key = {};
-    secrets."myservice/my_subdir/my_secret" = {
-      owner = config.users.users.michael.name;
-    }; */
+    age = {
+      keyFile = "/var/lib/sops-nix/key.txt";
+      # Set this to false since we're providing our own key
+      generateKey = false;
+    };
+    secrets."github/access_token" = { };
   };
 }
