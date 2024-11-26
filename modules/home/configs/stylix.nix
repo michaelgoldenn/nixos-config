178c5e4f2854dc50e2deeef8e44b8f5b59e115eb
michael <Michael.Golden0278@gmail.com>
2024-11-26 09:38:40 -0500

    umbriel 29 current  2024-11-26 09:38:28  24.11.20241018.4c2fcb0   6.6.57                          *

---
+++ modules/home/configs/stylix.nix
@@ -0,0 +1,14 @@
+{ flake, ... }:
+let
+  inherit (flake) config inputs;
+  inherit (inputs) self;
+in
+{
+  #stylix.targets.xyz.enable = false;
+  stylix = {
+    enable = true;
+    targets = {
+      firefox.profileNames = ["textfox"];
+    };
+  };
+}
