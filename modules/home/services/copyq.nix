4f086b1fecde053d24f108d7264030e4eea9c5bd
michael <Michael.Golden0278@gmail.com>
2024-11-27 13:15:18 -0500

    umbriel 54 current  2024-11-27 13:15:09  24.11.20241018.4c2fcb0   6.6.57                          *

--- modules/home/services/copyq.nix
+++ modules/home/services/copyq.nix
@@ -1,4 +1,16 @@
 { ... }:
 {
-  services.copyq.enable = true;
+  services.copyq = {
+    enable = true;
+  };
+  home.file.".config/copyq/copyq.conf".text = ''
+    [Options]
+    maxitems=2000
+    tabs=&clipboard
+    always_on_top=true
+    confirm_exit=false
+    close_on_unfocus=true
+    activate_pastes=false
+    disable_tray=false
+  '';
 }
