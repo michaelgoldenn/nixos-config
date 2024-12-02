4a3d1b749ea80fb09673b15c452c13e26e8d7344
michaelgoldenn <Michael.Golden0278@gmail.com>
2024-12-02 02:01:44 -0500

    titania 127 current  2024-12-02 02:01:32  24.11.20241018.4c2fcb0   6.6.57                          *

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -5,14 +5,14 @@ let
 in
 {
   imports = [/* inputs.sops-nix.nixosModules.sops */ inputs.sops-nix.nixosModules.default];
-  environment.systemPackages =  with pkgs; [  pinentry-curses ];
-/*   sops = {
+  environment.systemPackages =  with pkgs; [ pinentry-curses ];
+  sops = {
     defaultSopsFile = ../../secrets/example.yaml;
     age = {
-      keyFile = "/var/lib/sops-nix/key.txt";
+      keyFile = "/home/michael/.config/sops/age/keys.txt";
       # Set this to false since we're providing our own key
-      generateKey = false;
+      generateKey = true;
     };
     secrets."github/access_token" = { };
-  }; */
+  };
 }
