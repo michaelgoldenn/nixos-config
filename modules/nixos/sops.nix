3f0516f4a53e4dfa800e49fcbd28b931a7a36d63
michaelgoldenn <Michael.Golden0278@gmail.com>
2024-12-02 11:05:51 -0500

    titania 131 current  2024-12-02 11:05:33  24.11.20241018.4c2fcb0   6.6.57                          *

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -13,14 +13,19 @@ in
   imports = [inputs.sops-nix.nixosModules.sops /* inputs.sops-nix.nixosModules.default */];
   environment.systemPackages =  with pkgs; [ pinentry-curses ];
   sops = {
-    defaultSopsFile = ../../secrets/example.yaml;
+    defaultSopsFile = ../../secrets/secrets.yaml;
     age = {
       keyFile = "/home/michael/.config/sops/age/keys.txt";
       # Set this to false since we're providing our own key
       generateKey = true;
     };
     secrets = {
-      "github/access_token" = {
+      # I should really find if I can automatically set the owner and mode for all of them, instead of each individually
+      "github/nixos" = {
+        mode = "0440";
+        owner = config.users.users.michael.name;
+       };
+      "github/support-coop-game" = {
         mode = "0440";
         owner = config.users.users.michael.name;
        };
