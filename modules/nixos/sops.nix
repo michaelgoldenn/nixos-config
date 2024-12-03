7d4dee373364aeb745b764eb968bc74014f27431
michaelgoldenn <Michael.Golden0278@gmail.com>
2024-12-03 13:53:05 -0500

    sops stuff

--- modules/nixos/sops.nix
+++ modules/nixos/sops.nix
@@ -6,7 +6,7 @@ in
 { 
   # to add a new computer, run `mkdir -p ~/.config/sops/age`, then `age-keygen -o ~/.config/sops/age/keys.txt`.
   # then do `age-keygen -y ~/.config/sops/age/keys.txt` and put the output into ../../.sops.yaml
-  # then I think you need to run `nix-shell -p sops --run "sops updatekeys secrets/example.yaml"`
+  # then I think you need to run `nix-shell -p sops --run "sops updatekeys secrets/secrets.yaml"`
 
   # To add a new secret, `just sops` (or for nerds) `nix-shell -p sops --run "sops /etc/nixos/secrets/secrets.yaml"`and add it where you want
 
