dbacea6eadaeefae4ed40c33d6147f55dad3a1d0
michael <Michael.Golden0278@gmail.com>
2024-12-14 19:59:13 -0500

    hi

--- configurations/nixos/umbriel/configuration.nix
+++ configurations/nixos/umbriel/configuration.nix
@@ -51,7 +51,10 @@
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
-
+  # Workaround for GNOME autologin: https://github.com/NixOS/nixpkgs/issues/103746#issuecomment-945091229
+  systemd.services."getty@tty1".enable = false;
+  systemd.services."autovt@tty1".enable = false;
+  
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
