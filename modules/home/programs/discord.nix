5f4ecbe21ded4e07a1bf3687d6fdebd7e7ac7a48
michael <Michael.Golden0278@gmail.com>
2024-12-15 08:26:37 -0500

    umbriel 86 current  2024-12-15 08:26:14  25.05.20241203.55d15ad   6.6.63                          *

--- modules/home/programs/discord.nix
+++ modules/home/programs/discord.nix
@@ -4,32 +4,6 @@ let
   inherit (inputs) self;
 in
 {
-/*   imports = [inputs.nixcord.homeManagerModules.nixcord];
-  programs.nixcord = {
-    enable = true;  # also install discord
-    quickCss = "";
-    config = {
-      useQuickCss = true;
-      themeLinks = [
-        "https://raw.githubusercontent.com/link/to/some/theme.css"
-      ];
-      # Vencord options: https://github.com/KaylorBen/nixcord/blob/main/docs/vencord.md
-      frameless = true;
-
-      # Plugin Options: https://github.com/KaylorBen/nixcord/blob/main/docs/plugins.md
-      plugins = {
-        alwaysTrust = {
-          enable = true;
-          domain = true;
-          file = true;
-        };
-      };
-    };
-    extraConfig = {
-      # Some extra JSON config here
-      # ...
-    };
-  }; */
   home.packages = with pkgs; [
     vesktop
   ];
---
+++ modules/home/programs/nixcord.nix
@@ -0,0 +1,33 @@
+{ flake, pkgs, ... }:
+let
+  inherit (flake) config inputs;
+  inherit (inputs) self;
+in
+{
+  imports = [inputs.nixcord.homeManagerModules.nixcord];
+  programs.nixcord = {
+    enable = true;  # also install discord
+    quickCss = "";
+    config = {
+      useQuickCss = true;
+      themeLinks = [
+        "https://raw.githubusercontent.com/link/to/some/theme.css"
+      ];
+      # Vencord options: https://github.com/KaylorBen/nixcord/blob/main/docs/vencord.md
+      frameless = true;
+
+      # Plugin Options: https://github.com/KaylorBen/nixcord/blob/main/docs/plugins.md
+      plugins = {
+        alwaysTrust = {
+          enable = true;
+          domain = true;
+          file = true;
+        };
+      };
+    };
+    extraConfig = {
+      # Some extra JSON config here
+      # ...
+    };
+  };
+}
