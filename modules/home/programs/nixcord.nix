8b35e9cc501f7bc92db447864a179e5b7a09f2b7
michael <Michael.Golden0278@gmail.com>
2024-12-17 13:11:52 -0500

    umbriel 114 current  2024-12-17 13:11:32  25.05.20241213.3566ab7   6.6.64                          *

--- modules/home/programs/nixcord.nix
+++ modules/home/programs/nixcord.nix
@@ -18,7 +18,112 @@ in
 
       # Plugin Options: https://github.com/KaylorBen/nixcord/blob/main/docs/plugins.md
       plugins = {
-
+        alwaysExpandRoles.enable = true;
+        alwaysTrust = {
+          enable = true;
+          domain = true;
+          file = true;
+        };
+        betterSessions = {
+          enable = true;
+        };
+        betterSettings = {
+          enable = true;
+        };
+        callTimer = {
+          enable = true;
+        };
+        clearURLs = {
+          enable = true;
+        };
+        copyEmojiMarkdown = {
+          enable = true;
+        };
+        copyFileContents = {
+          enable = true;
+        };
+        crashHandler = {
+          enable = true;
+        };
+        customIdle = {
+          enable = true;
+        };
+        disableCallIdle.enable = false;
+        emoteCloner.enable = true;
+        fakeNitro = {
+          enable = true;
+        };
+        fixSpotifyEmbeds = {
+          enable = true;
+        };
+        fixYoutubeEmbeds = {
+          enable = true;
+        };
+        fullSearchContext.enable = true;
+        gameActivityToggle = {
+          enable = true;
+        };
+        imageZoom = {
+          enable = true;
+        };
+        keepCurrentChannel = {
+          enable = true;
+        };
+        mentionAvatars = {
+          enable = true;
+        };
+        messageLogger = {
+          enable = true;
+        };
+        noF1 = {
+          enable = true;
+        };
+        noOnboardingDelay.enable = true;
+        noReplyMention = {
+          enable = true;
+        };
+        normalizeMessageLinks.enable = true;
+        noTypingAnimation = {
+          enable = true;
+        };
+        noUnblockToJump.enable = true;
+        onePingPerDM = {
+          enable = true;
+        };
+        openInApp = {
+          enable = true;
+        };
+        replyTimestamp.enable = true;
+        revealAllSpoilers.enable = true;
+        reviewDB = {
+          enable = true;
+        };
+        sendTimestamps = {
+          enable = true;
+        };
+        silentTyping = {
+          enable = true;
+        };
+        spotifyCrack = {
+          enable = true;
+        };
+        themeAttributes = {
+          enable = true;
+        };
+        unindent.enable = true;
+        userMessagesPronouns = {
+          enable = true;
+        };
+        USRBG = {
+          enable = true;
+        };
+        validReply.enable = true;
+        validUser.enable = true;
+        voiceChatDoubleClick.enable = true;
+        volumeBooster = {
+          enable = true;
+        };
+        youtubeAdblock.enable = true;
       };
     };
     extraConfig = {
