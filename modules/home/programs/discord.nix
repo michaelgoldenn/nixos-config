8636de87656b24d30d0bd73f2b3e0616ed08368b
michael <Michael.Golden0278@gmail.com>
2024-12-17 13:15:46 -0500

    umbriel 115 current  2024-12-17 13:15:25  25.05.20241213.3566ab7   6.6.64                          *

--- modules/home/programs/discord.nix
+++ modules/home/programs/discord.nix
@@ -4,7 +4,131 @@ let
   inherit (inputs) self;
 in
 {
-  home.packages = with pkgs; [
-    vesktop
-  ];
+  imports = [inputs.nixcord.homeManagerModules.nixcord];
+  programs.nixcord = {
+    enable = true;  # also install discord
+    quickCss = "";
+    config = {
+      useQuickCss = true;
+      themeLinks = [
+        "https://raw.githubusercontent.com/link/to/some/theme.css"
+      ];
+      # Vencord options: https://github.com/KaylorBen/nixcord/blob/main/docs/vencord.md
+      frameless = true;
+
+      # Plugin Options: https://github.com/KaylorBen/nixcord/blob/main/docs/plugins.md
+      plugins = {
+        alwaysExpandRoles.enable = true;
+        alwaysTrust = {
+          enable = true;
+          domain = true;
+          file = true;
+        };
+        betterSessions = {
+          enable = true;
+        };
+        betterSettings = {
+          enable = true;
+        };
+        callTimer = {
+          enable = true;
+        };
+        clearURLs = {
+          enable = true;
+        };
+        copyEmojiMarkdown = {
+          enable = true;
+        };
+        copyFileContents = {
+          enable = true;
+        };
+        crashHandler = {
+          enable = true;
+        };
+        customIdle = {
+          enable = true;
+        };
+        disableCallIdle.enable = false;
+        emoteCloner.enable = true;
+        fakeNitro = {
+          enable = true;
+        };
+        fixSpotifyEmbeds = {
+          enable = true;
+        };
+        fixYoutubeEmbeds = {
+          enable = true;
+        };
+        fullSearchContext.enable = true;
+        gameActivityToggle = {
+          enable = true;
+        };
+        imageZoom = {
+          enable = true;
+        };
+        keepCurrentChannel = {
+          enable = true;
+        };
+        mentionAvatars = {
+          enable = true;
+        };
+        messageLogger = {
+          enable = true;
+        };
+        noF1 = {
+          enable = true;
+        };
+        noOnboardingDelay.enable = true;
+        noReplyMention = {
+          enable = true;
+        };
+        normalizeMessageLinks.enable = true;
+        noTypingAnimation = {
+          enable = true;
+        };
+        noUnblockToJump.enable = true;
+        onePingPerDM = {
+          enable = true;
+        };
+        openInApp = {
+          enable = true;
+        };
+        replyTimestamp.enable = true;
+        revealAllSpoilers.enable = true;
+        reviewDB = {
+          enable = true;
+        };
+        sendTimestamps = {
+          enable = true;
+        };
+        silentTyping = {
+          enable = true;
+        };
+        spotifyCrack = {
+          enable = true;
+        };
+        themeAttributes = {
+          enable = true;
+        };
+        unindent.enable = true;
+        userMessagesPronouns = {
+          enable = true;
+        };
+        USRBG = {
+          enable = true;
+        };
+        validReply.enable = true;
+        validUser.enable = true;
+        voiceChatDoubleClick.enable = true;
+        volumeBooster = {
+          enable = true;
+        };
+        youtubeAdblock.enable = true;
+      };
+    };
+    extraConfig = {
+      # Some extra JSON config here
+      # ...
+    };
+  };
 }
