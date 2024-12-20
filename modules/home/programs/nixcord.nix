37e1418ee81c99a892eb326756f2ee0627bf24dc
michaelgoldenn <Michael.Golden0278@gmail.com>
2024-12-20 11:07:40 -0500

    titania 174 current  2024-12-20 11:07:18  25.05.20241217.d3c42f1   6.6.66                          *

--- modules/home/programs/nixcord.nix
+++ modules/home/programs/nixcord.nix
@@ -6,7 +6,7 @@ in
 {
   imports = [inputs.nixcord.homeManagerModules.nixcord];
   programs.nixcord = {
-    enable = false;  # also installs discord
+    enable = true;  # also installs discord
     quickCss = "";
     config = {
       useQuickCss = true;
