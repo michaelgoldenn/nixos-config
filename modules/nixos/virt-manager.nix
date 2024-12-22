4041469ede90793b420c0d5fade8e7d570a8fbb7
michael <Michael.Golden0278@gmail.com>
2024-12-22 16:59:38 -0500

    mirandia 43 current  2024-12-22 16:59:32  25.05.20241217.d3c42f1   6.6.66                          *

--- modules/nixos/virt-manager.nix
+++ modules/nixos/virt-manager.nix
@@ -5,7 +5,7 @@ let
 
   hostConfigs = {
     mirandia = {
-      bridgeInterfaces = [ "enp9s0" ];
+      bridgeInterfaces = [ "enp0s20u2c2" ];
       ipAddress = "10.0.0.159"; #"192.168.1.132";
       needsBridge = true;  # Flag to indicate this host needs bridge config
     };
