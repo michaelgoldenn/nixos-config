90372a0bad01b84fb50e928fbbf35c0864ba3454
michael <Michael.Golden0278@gmail.com>
2024-12-22 17:28:09 -0500

    mirandia 47 current  2024-12-22 17:26:34  25.05.20241217.d3c42f1   6.6.66                          *

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -70,6 +70,7 @@ in
     libratbag
   ];
   virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.guest.enable = true;
   users.extraGroups.vboxusers.members = [ "michael" ];
 
   environment.shells = with pkgs; [ nushell ];
