37025069e050c3411e4a8fb1379f3be230dbb60a
michael <Michael.Golden0278@gmail.com>
2025-01-13 13:14:49 -0500

    umbriel 167 current  2025-01-13 13:14:48  25.05.20250108.bffc22e  6.6.69                          *

--- modules/nixos/whoogle.nix
+++ modules/nixos/whoogle.nix
@@ -42,7 +42,7 @@ in
         WHOOGLE_ALT_WIKI = "";
         WHOOGLE_ALT_QUORA = "";
         */
-        WHOOGLE_CONFIG_ALTS = "1";
+        WHOOGLE_CONFIG_ALTS = "0";
         WHOOGLE_CONFIG_THEME = "system";
         WHOOGLE_CONFIG_URL = "https://whoogle.${config.networking.domain}";
         WHOOGLE_CONFIG_GET_ONLY = "1";
