155a29413b65983d3d67a688597c751422a666ea
michael <Michael.Golden0278@gmail.com>
2025-01-15 12:35:48 -0500

    umbriel 182 current  2025-01-15 12:35:45  25.05.20250112.ed4a395  6.6.71                          *

--- modules/home/terminal/nushell.nix
+++ modules/home/terminal/nushell.nix
@@ -17,49 +17,25 @@
         cd = "z";
       };
       # See the Nushell docs for more options.
+/*       conf = builtins.toJSON {
+          "$env.config" = {
+            show_banner = false;
+              name = "abbr";
+              modifier = "control";
+              keycode = "space";
+              mode = ["emacs"  "vi_normal" "vi_insert"];
+              event = [
+              { send = "menu"; name = "abbr_menu";}
+              { edit = "insertchar"; value = " ";}
+              ];
+
+              # TODO: get the Menus section from the extraConfig into this toJSON struct
+          };
+      }; */
       extraConfig = ''
         $env.config = {
           show_banner: false
         }
-
-        # Keybindings
-        $env.config = ($env.config | upsert keybindings [
-          {
-            name: "abbr"
-            modifier: "none"
-            keycode: "space"
-            mode: ["emacs" "vi_normal" "vi_insert"]
-            event: [
-              { send: "menu" name: "abbr_menu" }
-              { edit: "insertchar" value: " " }
-            ]
-          }
-        ])
-
-        # Menus
-        $env.config = ($env.config | upsert menus [
-          {
-            name: "abbr_menu"
-            only_buffer_difference: false
-            marker: "👀 "
-            type: {
-              layout: "columnar"
-              columns: 1
-              col_width: 20
-              col_padding: 2
-            }
-            style: {
-              text: "green"
-              selected_text: "green_reverse"
-              description_text: "yellow"
-            }
-            source: { |buffer, position|
-              scope aliases
-              | where name == $buffer
-              | each { |elt| {value: $elt.expansion }}
-            }
-          }
-        ])
       '';
    };  
     carapace = {
