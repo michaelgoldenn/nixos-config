54717021e21c22a50e244297084bcaa9694fc6d3
michael <Michael.Golden0278@gmail.com>
2025-01-16 13:38:56 -0500

    umbriel 184 current  2025-01-16 13:38:53  25.05.20250112.ed4a395  6.6.71                          *

--- modules/home/terminal/nushell.nix
+++ modules/home/terminal/nushell.nix
@@ -19,46 +19,46 @@
       extraConfig = let
         config = {
           show_banner = false;
-          keybindings = [
-            {
-              name = "abbr";
-              modifier = "control";
-              keycode = "space";
-              mode = ["emacs" "vi_normal" "vi_insert"];
-              event = [
-                {
-                  send = "menu";
-                  name = "abbr_menu";
-                }
-                {
-                  edit = "insertchar";
-                  value = " ";
-                }
-              ];
-            }
-          ];
-          menus = [
-            {
-              name = "abbr_menu";
-              only_buffer_difference = false;
-              marker = "👀 ";
-              type = {
-                layout = "columnar";
-                columns = 1;
-                col_width = 20;
-                col_padding = 2;
-              };
-              style = {
-                text = "green";
-                selected_text = "green_reverse";
-                description_text = "yellow";
-              };
-              source = ''|buffer, position|
-                scope aliases
-                | where name == $buffer
-                | each { |elt| {value: $elt.expansion }}'';
-            }
-          ];
+        #  keybindings = [
+        #    {
+        #      name = "abbr";
+        #      modifier = "control";
+        #      keycode = "space";
+        #      mode = ["emacs" "vi_normal" "vi_insert"];
+        #      event = [
+        #        {
+        #          send = "menu";
+        #          name = "abbr_menu";
+        #        }
+        #        {
+        #          edit = "insertchar";
+        #          value = " ";
+        #        }
+        #      ];
+        #    }
+        #  ];
+        #  menus = [
+        #    {
+        #      name = "abbr_menu";
+        #      only_buffer_difference = false;
+        #      marker = "👀 ";
+        #      type = {
+        #        layout = "columnar";
+        #        columns = 1;
+        #        col_width = 20;
+        #        col_padding = 2;
+        #      };
+        #      style = {
+        #        text = "green";
+        #        selected_text = "green_reverse";
+        #        description_text = "yellow";
+        #      };
+        #      source = ''|buffer, position|
+        #        scope aliases
+        #        | where name == $buffer
+        #        | each { |elt| {value: $elt.expansion }}'';
+        #    }
+        #  ];
         };
       in ''
         $env.config = ${builtins.toJSON config}
