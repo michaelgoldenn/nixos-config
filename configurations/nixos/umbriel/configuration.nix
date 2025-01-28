6b83b90db58cfb2671d992b283d0a459588e9497
michael <Michael.Golden0278@gmail.com>
2025-01-28 15:37:59 -0500

    umbriel 192 current  2025-01-28 15:37:58  25.05.20250120.7ba9030  6.6.72                          *

--- configurations/nixos/umbriel/configuration.nix
+++ configurations/nixos/umbriel/configuration.nix
@@ -48,11 +48,11 @@
 
   # Enable tlp to limit battery
   services.power-profiles-daemon.enable = false;
-  services.tlp = { # Does not work rn
+  services.tlp = {
     enable = true;
     settings = {
-      START_CHARGE_THRESH_BAT0=75;
-      STOP_CHARGE_THRESH_BAT0=80;
+      #START_CHARGE_THRESH_BAT0=75;
+      #STOP_CHARGE_THRESH_BAT0=80;
       START_CHARGE_THRESH_BAT1=75;
       STOP_CHARGE_THRESH_BAT1=80;
     };
