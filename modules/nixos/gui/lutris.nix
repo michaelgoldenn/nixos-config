9c8b7719b465cc538c4262109dda1977993d0839
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-02-09 17:28:35 -0500

    still trying to get lutris to work

--- modules/nixos/gui/lutris.nix
+++ modules/nixos/gui/lutris.nix
@@ -15,10 +15,20 @@ in
 
     environment.systemPackages = with pkgs; [
       lutris
+      r2modman
+      vulkan-tools        # For vulkaninfo diagnostic tool
+      vulkan-loader       # 64-bit Vulkan loader
+      vulkan-validation-layers
+      pkgsi686Linux.vulkan-loader  # 32-bit Vulkan loader
       # wine
       wineWowPackages.stable
       # EA Installer
       geckodriver
     ];
+
+    environment.variables = {
+      VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/nvidia_icd.json";
+      DISABLE_LAYER_MESA_ENABLE_TIMELINE_SEMAPHORE_1 = "1";
+    };
   };
 }
