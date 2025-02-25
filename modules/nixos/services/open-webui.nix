dc72ec68d0ecd81a1150b38837d6b654a226b4f2
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-02-24 20:07:58 -0500

    titania 123 current  2025-02-24 20:07:54  25.05.20250215.d2544e1  6.12.13                          *

--- modules/nixos/services/open-webui.nix
+++ modules/nixos/services/open-webui.nix
@@ -19,6 +19,13 @@ in {
     services.open-webui = {
       enable = true;
       package = nixpkgs-stable.legacyPackages.${pkgs.system}.open-webui;
+      environment = {
+        ANONYMIZED_TELEMETRY = "False";
+        DO_NOT_TRACK = "True";
+        SCARF_NO_ANALYTICS = "True";
+        OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
+        OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+      };
     };
   };
 }
