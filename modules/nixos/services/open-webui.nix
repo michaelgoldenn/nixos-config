ecbe5ad99a357965b02c831398ed5226f6dbec5c
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-02-24 20:25:19 -0500

    titania 124 current  2025-02-24 20:25:15  25.05.20250215.d2544e1  6.12.13                          *

--- modules/nixos/services/open-webui.nix
+++ modules/nixos/services/open-webui.nix
@@ -25,6 +25,9 @@ in {
         SCARF_NO_ANALYTICS = "True";
         OLLAMA_API_BASE_URL = "http://127.0.0.1:11434/api";
         OLLAMA_BASE_URL = "http://127.0.0.1:11434";
+        ENABLE_OPENAI_API="true";
+        OPENAI_API_BASE_URL="https://api.anthropic.com/v1";
+        OPENAI_API_KEY = config.sops.secrets."ai/anthropic".path;
       };
     };
   };
