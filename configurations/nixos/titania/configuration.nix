e5483038dc3c61aeae3e5b481621950096cc5eca
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-03-05 10:18:31 -0500

    titania 140 current  2025-03-05 10:18:24  25.05.20250224.3ef3f3a  6.12.16                          *

--- configurations/nixos/titania/configuration.nix
+++ configurations/nixos/titania/configuration.nix
@@ -129,7 +129,7 @@
     # accessible via `nvidia-settings`.
     nvidiaSettings = true;
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
   systemd.sleep.extraConfig = ''
