f1651639bf3ae8c8e456216d58fdffd803be90ae
michael <Michael.Golden0278@gmail.com>
2025-04-09 10:25:44 -0400

    umbriel 264 current  2025-04-09 10:25:43  25.05.20250405.42a1c96  6.12.21                          *

--- modules/home/terminal/nushell.nix
+++ modules/home/terminal/nushell.nix
@@ -38,16 +38,14 @@ in {
           $env.config = {
             show_banner: false,
             completions: {
-              case_sensitive: false  # case-sensitive completions
-              quick: true            # set to false to prevent auto-selecting completions
-              partial: true          # set to false to prevent partial filling of the prompt
-              algorithm: "fuzzy"     # prefix or fuzzy
+              case_sensitive: false
+              quick: true
+              partial: true
+              algorithm: "fuzzy"
               external: {
-                # set to false to prevent nushell looking into $env.PATH to find more suggestions
                 enable: true
-                # set to lower can improve completion performance at the cost of omitting some options
                 max_results: 100
-                completer: $carapace_completer  # use the function we defined above
+                completer: carapace_completer  # No $ prefix when referencing the function
               }
             }
           }
