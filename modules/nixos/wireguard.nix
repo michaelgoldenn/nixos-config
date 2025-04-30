a0e8f73904b9cc55eea05a78264e6d94ba911d6b
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-04-30 09:44:03 -0400

    titania 176 current  2025-04-30 09:43:58  25.05.20250424.f771eb4  6.12.24                          *

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -1,37 +1,26 @@
-{ lib, pkgs, ... }:
+{pkgs, ...}: {
+  networking.wg-quick.interfaces = let
+    server_ip = "casptech.net";
+  in {
+    wg0 = {
+      # IP address of this machine in the *tunnel network*
+      address = ["10.10.10.7/32"];
+      dns = ["10.6.24.1" "10.10.10.1"];
 
-let
-  server_ip = "casptech.net";
-in
-{
-  # Enable WireGuard
-  networking.firewall = {
-    allowedUDPPorts = [51820];
-    checkReversePath = false;
-  };
-  networking.wireguard.enable = true;
-  networking.wireguard.interfaces = {
-     #"wg0" is the network interface name. You can name the interface arbitrarily.
-     wgl0 = {
-      # Determines the IP address and subnet of the client's end of the tunnel interface.
-      ips = [ "10.10.10.7/32" ];
-      listenPort = 51820; # to match firewall allowedUDPPorts (without this wg uses random port numbers)
-      #mtu = 1360;
+      # To match firewall allowedUDPPorts (without this wg
+      # uses random port numbers).
+      listenPort = 51820;
+
+      # Path to the private key file.
       privateKeyFile = "/etc/nixos/modules/nixos/private.key";
-      peers = [{
-          # Public key of the server (not a file path).
+      peers = [
+        {
           publicKey = "50Wn74uprGinf0UmHxayezL4JBBZph/IbipNejyi+ic=";
-          # Forward all the traffic via VPN.
-          allowedIPs = [ "10.6.24.0/24" ];
-          # Or forward only particular subnets
-          #allowedIPs = [ "10.100.0.1" "11.111.11.0/22" ];
-          # Set this to the server IP and port.
-          #name = "peer1";
-          endpoint = "${server_ip}:51820";  #  ToDo: route to endpoint not automatically configured https://wiki.archlinux.org/index.php/WireGuard#Loop_routing https://discourse.nixos.org/t/solved-minimal-firewall-setup-for-wireguard-client/7577
-          # Send keepalives every 25 seconds. Important to keep NAT tables alive.
+          allowedIPs = ["10.6.24.0/24"];
+          endpoint = "${server_ip}:51820";
           persistentKeepalive = 25;
         }
       ];
-    }; # it’s not imperative but it does not know how to do it : sudo ip route add 11.111.11.111 via 192.168.1.11 dev wlo1 the ip adresse 11: external and 192: local.
+    };
   };
 }
