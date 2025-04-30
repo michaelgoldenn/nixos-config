15934d15766e36f6f28c287964322f0b4b83e1d7
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-04-30 09:23:21 -0400

    titania 173 current  2025-04-30 09:23:16  25.05.20250424.f771eb4  6.12.24                          *

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -4,40 +4,8 @@ let
   server_ip = "casptech.net";
 in
 {
-/*   networking.firewall.allowedUDPPorts = [ 51820 ];
-  networking.networkmanager.enable = true;
-  networking.networkmanager.ensureProfiles = {
-    profiles."wg0" = {
-      connection = {
-        id             = "wg0";
-        uuid           = wgUUID;
-        type           = "wireguard";
-        interface-name = "wg0";     # keeps the kernel ifname nice
-        autoconnect    = false;     # flip to true if you want auto-up
-      };
-
-      ipv4 = {                     # static tunnel address
-        address1      = "10.10.10.7/32";
-        method        = "manual";
-        never-default = true;      # don’t override the normal default route
-      };
-      ipv6.method = "ignore";
-
-      # --- interface settings --------------------------------------
-      wireguard = {
-        listen-port = 51820;
-        private-key = "/etc/nixos/modules/nixos/private.key";
-        private-key-flags = 2;     # tell NM that the key comes from an agent
-      };
-
-      "wireguard-peer.50Wn74uprGinf0UmHxayezL4JBBZph/IbipNejyi+ic=" = {
-        endpoint            = "${serverHost}:51820";
-        allowed-ips         = "10.6.24.0/24;";   # semicolon is required
-        persistent-keepalive = 25;
-      };
-    };
-  }; */
   # Enable WireGuard
+  networking.firewall.allowedUDPPorts = [51820];
   networking.wireguard.enable = true;
   networking.wireguard.interfaces = {
      #"wg0" is the network interface name. You can name the interface arbitrarily.
