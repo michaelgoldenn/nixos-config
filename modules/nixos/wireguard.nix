d540080d184d0529868c4c279b901824320e4531
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-04-29 23:15:47 -0400

    titania 165 current  2025-04-29 23:15:42  25.05.20250424.f771eb4  6.12.24                          *

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -1,11 +1,28 @@
-{
-  config,
-  pkgs,
-  lib,
-  ...
-}: 
-{
-  networking.wireguard = {
-    enable = true;
+{pkgs, ...}: {
+  networking.wg-quick.interfaces = let
+    server_ip = "casptech.net";
+  in {
+    wg0 = {
+      # IP address of this machine in the *tunnel network*
+      address = [
+        "10.10.10.7/32"
+      ];
+
+      # To match firewall allowedUDPPorts (without this wg
+      # uses random port numbers).
+      listenPort = 51820;
+
+      # Path to the private key file.
+      privateKeyFile = "./private.key";
+
+      peers = [
+        {
+          publicKey = "50Wn74uprGinf0UmHxayezL4JBBZph/IbipNejyi+ic=";
+          allowedIPs = ["10.6.24.0/24"];
+          endpoint = "${server_ip}:51820";
+          persistentKeepalive = 25;
+        }
+      ];
+    };
   };
 }
