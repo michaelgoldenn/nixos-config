f45928fafcd0ad0a855588202c987c5a75ad1776
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-05-01 19:30:32 -0400

    titania 179 current  2025-05-01 19:30:25  25.05.20250429.46e634b  6.12.25                          *

--- modules/nixos/wireguard.nix
+++ modules/nixos/wireguard.nix
@@ -1,8 +1,9 @@
 {pkgs, ...}: {
   networking.wg-quick.interfaces = let
-    server_ip = "casptech.net";
+    oliver_server_ip = "casptech.net";
+    michael_server_ip = "";
   in {
-    wg0 = {
+    oliver-server = {
       # IP address of this machine in the *tunnel network*
       address = ["10.10.10.7/32"];
       dns = ["10.6.24.1" "10.10.10.1"];
@@ -12,16 +13,26 @@
       listenPort = 51820;
 
       # Path to the private key file.
-      privateKeyFile = "/etc/nixos/modules/nixos/private.key";
+      privateKeyFile = "/etc/nixos/modules/nixos/oliver-server.private.key";
       peers = [
         {
           publicKey = "50Wn74uprGinf0UmHxayezL4JBBZph/IbipNejyi+ic=";
           allowedIPs = ["10.6.24.0/24"];
-          endpoint = "${server_ip}:51820";
+          endpoint = "${oliver_server_ip}:51820";
           persistentKeepalive = 25;
         }
       ];
     };
+/*     michaels-server = {
+      address = ["10.30.0.10/32"];
+      dns = "1.1.1.1";
+      privateKeyFile = "/etc/nixos/modules/nixos/michael-server.private.key";
+      peers = [{
+        publicKey = "";
+        allowedIPs = ["0.0.0.0/0" "::/0"];
+        endpoint = "${oliver_server_ip}:51820";
+        persistentKeepalive = 25;
+      }];
+    }; */
   };
-  
 }
