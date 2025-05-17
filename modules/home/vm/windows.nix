1b70a3d236b6bdf43e88f0b78b779c4569ed3dd9
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-05-17 17:55:09 -0400

    titania 187 current  2025-05-17 17:55:04  25.05.20250508.dda3dcd  6.12.26                          *

--- modules/home/vm/windows.nix
+++ modules/home/vm/windows.nix
@@ -1,20 +1,21 @@
-{
-  pkgs,
-  config,
-  lib,
-  ...
-}:
+{ pkgs, config, ... }:
+let
+  runWindows = pkgs.writeShellScriptBin "run-windows" ''
+    exec quickemu --vm "${config.home.homeDirectory}/system/vm/windows-11.conf" --display spice
+  '';
+in
 {
   home.packages = with pkgs; [
     quickemu
     spice-gtk
+    runWindows           # add the wrapper to $PATH
   ];
 
   xdg.desktopEntries.windows = {
-    name = "Windows VM";
+    name        = "Windows 11";
     genericName = "Windows VM";
-    exec = "quickemu --vm system/vm/windows-11.conf --display spice";
-    icon = "distributor-logo-windows";
-    terminal = false;
+    exec        = "run-windows";
+    icon        = "distributor-logo-windows";
+    terminal    = false;   # set to true if you want to see stdout/stderr
   };
 }
