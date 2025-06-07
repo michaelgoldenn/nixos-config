94118a1116d5658e387b2804b06782bddb0981fd
michael <Michael.Golden0278@gmail.com>
2025-06-07 18:05:29 -0400

    improved battery settings

--- configurations/nixos/umbriel/configuration.nix
+++ configurations/nixos/umbriel/configuration.nix
@@ -50,6 +50,11 @@
   services.tlp = {
     enable = true;
     settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+      
       START_CHARGE_THRESH_BAT0 = 75;
       STOP_CHARGE_THRESH_BAT0 = 80;
       START_CHARGE_THRESH_BAT1 = 75;
