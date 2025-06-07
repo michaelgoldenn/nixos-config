414055d867df8c0427904b7efd45f41fd168c4fe
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-06-06 20:22:32 -0400

    titania 224 current  2025-06-06 01:18:46  25.11.20250603.c2a0396  6.12.31                          *

--- modules/home/programs/IDEs/helix.nix
+++ modules/home/programs/IDEs/helix.nix
@@ -1,21 +1,53 @@
 {
   flake,
   ...
-}: let
+}:
+let
   inherit (flake) config inputs;
-in {
+in
+{
   programs.helix = {
     enable = true;
     languages = builtins.fromTOML (builtins.readFile ./helix-config/languages.toml);
     settings = {
-  keys.normal = {
-    "A-k" = ["goto_line_end" "extend_line_below" "delete_selection" "move_line_up" "paste_before"];
-    "A-j" = ["goto_line_end" "extend_line_below" "delete_selection" "paste_after"];
-  };
-  keys.select = {
-    "A-k" = ["goto_line_end" "extend_line_below" "delete_selection" "move_line_up" "paste_before" "select_mode"];
-    "A-j" = ["goto_line_end" "extend_line_below" "delete_selection" "paste_after" "select_mode"];
-  };
-};
+      editor = {
+        inline-diagnostics = {
+          cursor-line = "hint";
+          other-lines = "error";
+        };
+      };
+      keys.normal = {
+        "A-k" = [
+          "goto_line_end"
+          "extend_line_below"
+          "delete_selection"
+          "move_line_up"
+          "paste_before"
+        ];
+        "A-j" = [
+          "goto_line_end"
+          "extend_line_below"
+          "delete_selection"
+          "paste_after"
+        ];
+      };
+      keys.select = {
+        "A-k" = [
+          "goto_line_end"
+          "extend_line_below"
+          "delete_selection"
+          "move_line_up"
+          "paste_before"
+          "select_mode"
+        ];
+        "A-j" = [
+          "goto_line_end"
+          "extend_line_below"
+          "delete_selection"
+          "paste_after"
+          "select_mode"
+        ];
+      };
+    };
   };
 }
