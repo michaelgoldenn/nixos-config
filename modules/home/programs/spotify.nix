c4798a8e4ed2416e841ed16ab9bf7a1f2bd73cf4
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-06-11 20:13:49 -0400

    titania 241 current  2025-06-11 20:13:39  25.11.20250607.3e3afe5  6.12.32                          *

--- modules/home/programs/spotify.nix
+++ modules/home/programs/spotify.nix
@@ -9,28 +9,31 @@ in
     inputs.spicetify-nix.homeManagerModules.default
   ];
   programs.spicetify =
-   let
-     spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
-   in
-   {
-     enable = true;
-     #extensions are defined here: https://github.com/Gerg-L/spicetify-nix/blob/master/docs/EXTENSIONS.md
-     enabledExtensions = with spicePkgs.extensions; [
-       adblock
-       hidePodcasts
-       shuffle # shuffle+ (special characters are sanitized out of extension names)
-       history
-       showQueueDuration
-       autoVolume
-       songStats
-       beautifulLyrics
-       #oldSidebar
-     ];
-     #Themes are defined here: https://github.com/Gerg-L/spicetify-nix/blob/master/docs/THEMES.md
-     #Note: Not letting stylix handle spicetify stuff as I can't get custom themes with stylix atm.
-     #theme = spicePkgs.themes.text;
-     #colorScheme = "catppuccin-mocha";
-     #theme = spicePkgs.themes.catppuccin;
-     #colorScheme = "mocha";
-   };
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.system};
+    in
+    {
+      enable = true;
+      #extensions are defined here: https://github.com/Gerg-L/spicetify-nix/blob/master/docs/EXTENSIONS.md
+      enabledExtensions = with spicePkgs.extensions; [
+        adblock
+        hidePodcasts
+        shuffle # shuffle+ (special characters are sanitized out of extension names)
+        history
+        showQueueDuration
+        autoVolume
+        songStats
+        beautifulLyrics
+        #oldSidebar
+      ];
+      enabledCustomApps = with spicePkgs.apps; [
+        marketplace
+      ];
+      #Themes are defined here: https://github.com/Gerg-L/spicetify-nix/blob/master/docs/THEMES.md
+      #Note: Not letting stylix handle spicetify stuff as I can't get custom themes with stylix atm.
+      #theme = spicePkgs.themes.text;
+      #colorScheme = "catppuccin-mocha";
+      #theme = spicePkgs.themes.catppuccin;
+      #colorScheme = "mocha";
+    };
 }
