d5c9cf475065c450b5d0240f49e3cb0ab7709832
michael <Michael.Golden0278@gmail.com>
2025-09-18 14:49:44 -0400

    added IDEs, added options for terminal and shell

---
+++ modules/home/cli/helix/helix.nix
@@ -0,0 +1,28 @@
+## A Vim-Like editor written in Rust
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.helix.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "A Vim-Like editor written in Rust";
+  };
+
+  config = lib.mkIf config.helix.enable {
+    home.packages = with pkgs; [
+    ];
+    programs = {
+      helix = {
+        enable = true;
+        languages = builtins.fromTOML (builtins.readFile ./languages.toml) // {
+        };
+        settings = builtins.fromTOML (builtins.readFile ./config.toml) // {
+        };
+      };
+    };
+  };
+}
--- modules/home/cli/terminal/default.nix
+++ modules/home/cli/terminal/default.nix
@@ -1,8 +1,38 @@
-# A module that automatically imports everything else in the parent folder.
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  ## Terminal
+  options.terminal = {
+    default = lib.mkOption {
+      type = lib.types.enum [ "ghostty" ];
+      default = "ghostty";
+      description = "Default terminal emulator";
+    };
+  };
+  ## Shell
+  options.shell = {
+    default = lib.mkOption {
+      type = lib.types.enum [
+        "nushell"
+        "zsh"
+      ];
+      default = "nushell";
+      description = "Default shell";
+    };
+  };
+
+  # import everything in sub-folder
   imports =
     with builtins;
-    map
-      (fn: ./${fn})
-      (filter (fn: fn != "default.nix") (attrNames (readDir ./.)));
+    map (fn: ./${fn}) (filter (fn: fn != "default.nix") (attrNames (readDir ./.)));
+  ## Set Terminal
+  home.packages = [ (pkgs.${config.opt.terminal.default}) ];
+  environment.sessionVariables.TERMINAL = config.opt.terminal.default;
+  ## Set Shell
+  nushell.enable = config.opt.shell == "nushell";
+  zsh.enable = config.opt.shell == "zsh";
 }
