4b34be6ecffcaa3880486b41c3b11b7c52c8d2fd
michaelgoldenn <Michael.Golden0278@gmail.com>
2025-09-18 09:15:52 -0400

    adding stylix

---
+++ modules/home/background/stylix.nix
@@ -0,0 +1,12 @@
+{}:
+{
+  stylix = {
+    enable = true;
+    targets = {
+      firefox.enable = true;
+      firefox.profileNames = [ "textfox" ];
+      spicetify.enable = false;
+      vscode.profileNames = [ "default" ];
+    };
+  };
+}
