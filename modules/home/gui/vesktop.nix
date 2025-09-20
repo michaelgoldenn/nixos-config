ed060ad35450a891b58b309135fe6025cc6fea4d
michael <Michael.Golden0278@gmail.com>
2025-09-20 17:26:42 -0400

    improved vesktop file

--- modules/home/gui/vesktop.nix
+++ modules/home/gui/vesktop.nix
@@ -1,6 +1,15 @@
-{pkgs, ...}:
+## A Discord client with many extensions
+{ config, lib, ... }:
 {
+  options.vesktop.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "A Discord client with many extensions";
+  };
+
+  config = lib.mkIf config.vesktop.enable {
   programs.vesktop = {
     enable = true;
   };
+};
 }
