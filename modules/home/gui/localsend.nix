16286c848f63fc4a5e35c2923e54e7b252b760e1
michael <Michael.Golden0278@gmail.com>
2025-09-24 22:48:46 -0400

    added localsend

---
+++ modules/home/gui/localsend.nix
@@ -0,0 +1,15 @@
+## An app to send to other compters on the local network
+{ config, lib, pkgs, ... }:
+{
+  options.localsend.enable = lib.mkOption {
+    type = lib.types.bool;
+    default = true;
+    description = "An app to send to other compters on the local network";
+  };
+
+  config = lib.mkIf config.localsend.enable {
+  home.packages = with pkgs; [
+    localsend
+  ];
+};
+}
