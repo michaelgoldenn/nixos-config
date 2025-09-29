d9c4208a6489bfdf23728a1977b3fdb337233df8
michael <Michael.Golden0278@gmail.com>
2025-09-29 12:27:24 -0400

    use nixpkgs obsidian rather than home-manager

--- modules/home/gui/obsidian.nix
+++ modules/home/gui/obsidian.nix
@@ -13,15 +13,18 @@
   };
 
   config = lib.mkIf config.obsidian.enable {
-    programs.obsidian = {
-      enable = true;
-      vaults = {
-        main-vault = {
-          enable = true;
-          target = "Documents/obsidian-vault";
-        };
-      };
-    };
+    home.packages = [ pkgs.obsidian ];
+    
+    # Don't use home-manager for now, it forces you to declaritively define everything (https://github.com/nix-community/home-manager/issues/7906)
+    # programs.obsidian = {
+    #   enable = true;
+    #   vaults = {
+    #     main-vault = {
+    #       enable = true;
+    #       target = "Documents/obsidian-vault";
+    #     };
+    #   };
+    # };
     # stylix.targets.obsidian.vaultNames = [ "main-vault" ]; # add main vault to stylix
   };
 }
