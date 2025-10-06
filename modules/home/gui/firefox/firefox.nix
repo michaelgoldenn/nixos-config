7e7aa1f567c8b5a555e5c7b87d4cecf6552b9432
michael <Michael.Golden0278@gmail.com>
2025-10-06 12:42:09 -0400

    fixed firefox warnings

--- modules/home/gui/firefox/firefox.nix
+++ modules/home/gui/firefox/firefox.nix
@@ -328,6 +328,7 @@ in
   imports = [ inputs.textfox.homeManagerModules.default ];
   textfox = {
     enable = true;
+    useLegacyExtensions = false;
     profile = "textfox";
     config = {
       background = {
