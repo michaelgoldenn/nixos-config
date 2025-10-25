0bef7cd079668efe22ccf00e206b40702abc017f
Michael Golden <Michael.Golden0278@gmail.com>
2025-10-24 20:28:56 -0400

    added video editing module

---
+++ modules/home/gui/video_editing.nix
@@ -0,0 +1,17 @@
+## Enables software used to edit videos and such
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+{
+  options.video_editing.enable = lib.mkEnableOption "video_editing";
+
+  config = lib.mkIf config.video_editing.enable {
+    home.packages = with pkgs; [
+      davinci-resolve
+      audacity
+    ];
+  };
+}
