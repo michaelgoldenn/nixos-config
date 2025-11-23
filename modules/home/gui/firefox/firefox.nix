a46658813777058cab0119610dec187ffc0ab15e
michael <Michael.Golden0278@gmail.com>
2025-11-23 14:13:53 -0500

    set firefox to be default for html

--- modules/home/gui/firefox/firefox.nix
+++ modules/home/gui/firefox/firefox.nix
@@ -109,6 +109,17 @@ let
   shyfox_sidebery_config = builtins.fromJSON (builtins.readFile ./shyfox-sidebery-settings.json);
 in
 {
+  # set firefox to be default for html
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "firefox.desktop";
+      "x-scheme-handler/http" = "firefox.desktop";
+      "x-scheme-handler/https" = "firefox.desktop";
+      "x-scheme-handler/about" = "firefox.desktop";
+      "x-scheme-handler/unknown" = "firefox.desktop";
+    };
+  };
   programs.firefox = {
     enable = true;
     profiles = {
