72bc0e9fe91b248755896ce5bd1a5aa48a1152f4
michael <Michael.Golden0278@gmail.com>
2025-11-23 19:05:50 -0500

    enabled 32 bit graphics for umbriel

--- configurations/nixos/umbriel/configuration.nix
+++ configurations/nixos/umbriel/configuration.nix
@@ -134,6 +134,7 @@
   # Enable OpenGL
   hardware.graphics = {
     enable = true;
+    enable32Bit = true;
     extraPackages = with pkgs; [
       # 64‑bit VA‑API libs
       intel-media-driver # modern “iHD” driver (Gen 9 → Arc)
